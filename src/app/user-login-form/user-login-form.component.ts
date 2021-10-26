ff26de99ba21237abf81bb067f4707c772f23c05
Ellen Studer <ellenmstuder@gmail.com>
2021-10-25 22:32:06 -0500

    add welcome page, implement routing, format the movie-view

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,36 +10,37 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
-
+  
   @Input() userData = { Username: '', Password: '' };
-
+  
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public MatSnackBar: MatSnackBar
-  ) { }
-
-  ngOnInit(): void {
-  }
-
-  // function for sending form inputs to backend 
-  loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
-      // [[ logic for successful user login goes here! TBI ]]
-      this.dialogRef.close(); //close the modal on success
-      console.log(response);
-      localStorage.setItem('user', response.user.Username);
-      localStorage.setItem('token', response.token);
-      console.log('local storage (login): ', localStorage)
-      this.MatSnackBar.open('user logged in successfully!', 'OK', {
-        duration: 2000
+    public MatSnackBar: MatSnackBar,
+    public router: Router
+    ) { }
+    
+    ngOnInit(): void {
+    }
+    
+    // function for sending form inputs to backend 
+    loginUser(): void {
+      this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+        // [[ logic for successful user login goes here! TBI ]]
+        this.dialogRef.close(); //close the modal on success
+        console.log(response);
+        localStorage.setItem('user', response.user.Username);
+        localStorage.setItem('token', response.token);
+        console.log('local storage (login): ', localStorage)
+        this.MatSnackBar.open('user logged in successfully!', 'OK', {
+          duration: 2000
+        });
+      }, (response) => {
+        console.log(response);
+        this.MatSnackBar.open(response, 'OK', {
+          duration: 2000
+        });
       });
-    }, (response) => {
-      console.log(response);
-      this.MatSnackBar.open(response, 'OK', {
-        duration: 2000
-      });
-    });
-  }
-
+      this.router.navigate(['movies']);
+    }
 }
